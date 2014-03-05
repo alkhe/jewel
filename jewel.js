c1c23521f8a004825716c708be9cda18fe4f4830
root--- <sort@programmer.net>
2014-03-04 22:33:58 -0500

    Namespaces?

--- jewel.js
+++ jewel.js
@@ -1,47 +1,62 @@
-function JControl() {
-	this.__identifier = "control";
-	this.__text = "";
-}
+var Jewel = {
+	JControl: function() {
+		this.__identifier = "control";
+		this.__text = "";
+	}
 
-function JButton() {
-	this.__type = "button";
-	this.__extra = "type='button'";
-	this.__options = "";
-	this.__text = "";
-	this.setText = function(textString) {
-		this.__text = textString;
-	};
-	this.html = function() {
-		return "<" + this.__type + " " + this.__extra + " " + this.__options + ">" + this.__text + "</" + this.__type + ">";
-	}
-}
-
-function JViewport() {
-	this.__controls = new Array();
-	this.add = function(control) {
-		this.__controls.push(control);
-	}
-	this.invalidate = function() {
-		this.__paint();
-	}
-	this.__paint = function() {
-		var content = "";
-		var arrayLength = this.__controls.length;
-		for (var i = 0; i < arrayLength; i++) {
-			content += this.__controls[i].html();
+	JButton:: function() {
+		this.__type = "button";
+		this.__extra = "type='button'";
+		this.__options = "";
+		this.__text = "";
+		this.setText = function(textString) {
+			this.__text = textString;
+		};
+		this.html = function() {
+			return "<" + this.__type + " " + this.__extra + " " + this.__options + ">" + this.__text + "</" + this.__type + ">";
 		}
-		paintBody(content);
 	}
-}
 
-function paintBody(textString) {
-	document.body.innerHTML = textString;
+	JTextBox: function() {
+		this.__type = "button";
+		this.__extra = "type='button'";
+		this.__options = "";
+		this.__text = "";
+		this.setText = function(textString) {
+			this.__text = textString;
+		};
+		this.html = function() {
+			return "<" + this.__type + " " + this.__extra + " " + this.__options + ">" + this.__text + "</" + this.__type + ">";
+		}
+	}
+
+	JViewport: function() {
+		this.__controls = new Array();
+		this.add = function(control) {
+			this.__controls.push(control);
+		}
+		this.invalidate = function() {
+			this.__paint();
+		}
+		this.__paint = function() {
+			var content = "";
+			var arrayLength = this.__controls.length;
+			for (var i = 0; i < arrayLength; i++) {
+				content += this.__controls[i].html();
+			}
+			paintBody(content);
+		}
+	}
+
+	paintBody: function(textString) {
+		document.body.innerHTML = textString;
+	}
+
+	Load: function() {
+
+	}
 }
 
 window.onload = function() {
-	load();
+	Jewel.Load();
 };
-
-function load() {
-
-}
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function load() {
-	var doc = new JViewport();
-	var btn = new JButton();
+Jewel.Load = function() {
+	var doc = new Jewel.Viewport();
+	var btn = new Jewel.Button();
 	btn.setText("Button");
 	doc.add(btn);
+	var btn2 = new Jewel.Button();
+	btn2.setText("Button");
+	doc.add(btn2);
 	doc.invalidate();
 }
+
