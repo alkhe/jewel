14766cddfcca9b1b75868270bffa9583f08da457
Pokecommunity <pokecommunity@programmer.net>
2014-03-04 21:50:18 -0500

    Initial commit

---
+++ jewel.js
@@ -0,0 +1,39 @@
+function JControl() {
+	this.__identifier = "control";
+	this.__text = "";
+}
+
+function JButton() {
+	this.__type = "button";
+	this.__extra = "type='button'";
+	this.__options = "";
+	this.__text = "";
+	this.setText = function(textString) {
+		this.__text = textString;
+	};
+	this.html = function() {
+		return "<" + this.__type + " " + this.__extra + " " + this.__options + ">" + this.__text + "</" + this.__type + ">";
+	}
+}
+
+function JViewport() {
+	this.__controls = new Array();
+	this.add = function(control) {
+		this.__controls.push(control);
+	}
+	this.invalidate = function() {
+		this.__paint();
+	}
+	this.__paint = function() {
+		var content = "";
+		var arrayLength = this.__controls.length;
+		for (var i = 0; i < arrayLength; i++) {
+			content += this.__controls[i].html();
+		}
+		paintBody(content);
+	}
+}
+
+function paintBody(textString) {
+	document.body.innerHTML = textString;
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	var doc = new JViewport();
+	var btn = new JButton();
+	btn.setText("Button");
+	doc.add(btn);
+	doc.invalidate();
+}
