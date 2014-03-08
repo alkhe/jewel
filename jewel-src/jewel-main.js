4766b6a87d899d9a19628636608c66aa56bce765
root--- <sort@programmer.net>
2014-03-07 20:10:36 -0500

    Updates

---
+++ jewel-src/jewel-main.js
@@ -0,0 +1,40 @@
+window.addEventListener("load",function() {
+	Jewel.Load();
+});
+
+var Jewel = function(callback) {
+	Jewel.__callback = callback;
+	Jewel.__viewports = [];
+};
+
+Jewel.Load = function() {
+	Jewel.__callback();
+};
+
+Jewel.Add = function(new_viewport) {
+	if (Jewel.__viewports.indexOf(new_viewport)<0) {
+		Jewel.__viewports.push(new_viewport);
+	}
+};
+
+Jewel.Paint = function() {
+	console.log("Paint");
+	var viewports = Jewel.__viewports;
+	for (var i=0,l=viewports.length; i<l; i++) {
+		if (document.body.contains(viewports[i].__element)) {
+			document.body.removeChild(viewports[i].__element);
+		}
+		viewports[i].Paint();
+		
+		if (!document.body.contains(viewports[i].__element)) {
+			document.body.appendChild(viewports[i].__element);
+		}
+	}
+};
+
+Jewel.Update = function() {
+	var viewports = Jewel.__viewports;
+	for (var i=0,l=viewports.length; i<l; i++) {
+		viewports[i].Update();
+	}
+};
